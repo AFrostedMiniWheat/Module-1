ec07aa3bdbcd1c1a29cbb8ce35a0aaf7ea35316d
Jason AKA Thug Nasty <yeabuddyxD@gmail.com>
2020-01-27 23:55:14 -0500

    Java Files only for simplicity :)
    Signed-off-by: Jason AKA Thug Nasty <yeabuddyxD@gmail.com>

---
+++ Chapter 1 (Java Files Only)/Ch1_5.java
@@ -0,0 +1,10 @@
+
+public class Ch1_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = ((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+		System.out.println("Your value will be " +a);
+	}
+
+}
